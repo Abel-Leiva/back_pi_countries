6a32ca9de1c43ebf044d40a72f18d44a7578afb8
Abel Leiva <110953504+Abel-Leiva@users.noreply.github.com>
2025-06-09 01:39:44 -0300

    Update loadCountriesToDatabase.js

--- src/utils/loadCountriesToDatabase.js
+++ src/utils/loadCountriesToDatabase.js
@@ -3,18 +3,25 @@ const axios = require("axios");
 
 const loadCountriesToDatabase = async () => {
   try {
-   const response = await axios.get(
-  `https://restcountries.com/v3.1/all?fields=cca3,name,flags,continents,capital,subregion,area,population`
-);
+    const response = await axios.get(
+      `https://restcountries.com/v3.1/all?fields=cca3,name,flags,continents,capital,subregion,area,population`
+    );
 
     const countries = response.data;
-    //
+
     const promises = countries.map(async (country) => {
+      // Validar que la bandera exista, si no, poner una imagen por defecto o saltar
+      const flagImage = country.flags?.png || null;
+
+      if (!flagImage) {
+        console.warn(`País sin bandera PNG: ${country.name.common}`);
+      }
+
       const [instance] = await Country.findOrCreate({
         where: { id: country.cca3 },
         defaults: {
           name: country.name.common,
-          imageFlag: country.flags[1],
+          imageFlag: flagImage,
           continent: country.continents?.join(", "),
           capital: country.capital ? country.capital.join(", ") : "no tiene",
           subRegion: country.subregion,
@@ -22,11 +29,12 @@ const loadCountriesToDatabase = async () => {
           population: country.population,
         },
       });
+
       return instance.toJSON();
     });
 
-    // que se completen todas las promesas
     const createdCountries = await Promise.all(promises);
+    console.log(`Cargados ${createdCountries.length} países a la base de datos.`);
   } catch (error) {
     throw new Error(
       `Error al cargar los países a la base de datos: ${error.message}`
@@ -38,4 +46,3 @@ module.exports = {
   loadCountriesToDatabase,
 };
 
-//
